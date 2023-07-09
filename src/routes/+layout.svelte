3b8112adf0a5aa0c2dae06e3d7939252e129dd6e
ippsav <mehdiboujid@gmail.com>
2023-07-09 13:03:14 +0100

    update: change main color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@ import Navbar from '$lib/components/Navbar.svelte';
 	import './styles.css';
 </script>
 
-<div class="app dark:bg-gray-900">
+<div class="app dark:bg-gray-900 bg-orange-100">
   <Navbar />
   <slot />
   <Footer />
