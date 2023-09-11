c14db359164b003b9b9ccb86d2ad95b24cdee8d3
ippsav <mehdiboujid@gmail.com>
2023-09-11 17:10:44 +0100

    update: wording

--- src/lib/components/IntroductionHeader.svelte
+++ src/lib/components/IntroductionHeader.svelte
@@ -5,9 +5,8 @@
 		I am a software engineer specializing in full-stack development, with a primary focus on
 		backend technologies.<br/>
     I have experience working with various languages and technologies such as
-		Golang, Rust, Node.js (JavaScript/TypeScript), Vue, React, Docker, and Google Cloud. My skill
-		set allows me to effectively deliver robust solutions and handle diverse software development
-		challenges. I continuously seek to enhance my expertise in the field and remain dedicated to
+		Golang, Rust, Node.js (JavaScript/TypeScript), Vue, React, Docker, and Google Cloud.
+    I continuously seek to enhance my expertise in the field and remain dedicated to
 		delivering high-quality results.
 	</p>
 </div>
