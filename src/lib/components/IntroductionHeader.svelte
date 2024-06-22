1645d851b24a98ae377c333f09e481ac95120313
ippsav <mehdiboujid@gmail.com>
2024-06-23 00:41:49 +0100

    small change

--- src/lib/components/IntroductionHeader.svelte
+++ src/lib/components/IntroductionHeader.svelte
@@ -2,11 +2,10 @@
 	<h1 class="text-4xl font-bold">Mehdi Boujid</h1>
 	<p>Software engineer</p>
 	<p class="text-sm max-w-2xl text-center mt-2">
-		I am a software engineer specializing in full-stack development, with a primary focus on
-		backend technologies.<br/>
-    I have experience working with various languages and technologies such as
-		Golang, Rust, Node.js (JavaScript/TypeScript), Vue, React, Docker, and Google Cloud.
-    I continuously seek to enhance my expertise in the field and remain dedicated to
-		delivering high-quality results.
+		I am a software engineer specialized in backend technologies. Recently, I've been developing a
+		growing interest in low-level computing concepts.<br />
+		I have experience working with various languages and technologies such as Golang, Rust, Node.js (JavaScript/TypeScript),
+		Vue, React, Docker, and Google Cloud. I continuously seek to enhance my expertise in the field and
+		remain dedicated to delivering high-quality results.
 	</p>
 </div>
