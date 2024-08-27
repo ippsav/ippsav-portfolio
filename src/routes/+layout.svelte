c1049cde8bf35f9fbbe5bc5bba2c7d02b32b6e22
ippsav <mehdiboujid@gmail.com>
2024-08-27 03:21:22 +0100

    move style.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import Footer from '$lib/components/Footer.svelte';
   import Navbar from '$lib/components/Navbar.svelte';
-  import './styles.css';
+  import '../styles.css';
 </script>
 
 <div class="app bg-white dark:bg-black text-green-700 dark:text-green-500 min-h-screen flex flex-col">
