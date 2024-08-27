d76845ab6f8fd0cfde6aa7b9f89b67697104d744
ippsav <mehdiboujid@gmail.com>
2024-08-27 01:02:59 +0100

    feat(uptime): add work duration

--- src/lib/components/TimelineItem.svelte
+++ src/lib/components/TimelineItem.svelte
@@ -12,6 +12,10 @@
 				<span class="text-black dark:text-white ml-2">{timeline.title} @ {timeline.company}</span>
 			</div>
 	</div>
+	<div class="ml-4 mt-1">
+		<span class="text-green-700 dark:text-green-500">$ uptime</span>
+		<span class="ml-2 text-blue-600 dark:text-blue-400">{timeline.duration}</span>
+	</div>
 	<div class="ml-4 text-gray-700 dark:text-gray-400 whitespace-pre-line mt-2">{timeline.details}</div>
 	<TagList tags={timeline.stack} label="skills" />
 </div>
